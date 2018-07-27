6a0a338d8bbbbbc1255266bc192c030d9b85547d
Jacob Wiltse <aquabug@gmail.com>
2018-07-27 15:31:12 -0400

    Add some initial models, add Pipfile, and Procfile for heroku

--- game/models.py
+++ game/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Acs(models.Model):
+    name = models.CharField(max_length=50, null=True, default=None)
+    target = models.PositiveIntegerField()
+    arrival = models.IntegerField(null=True, default=None)
+
+
+class Alliance(models.Model):
+    ally_name = models.CharField(max_length=50, default='')
+    ally_tag = models.CharField(max_length=20, default='')
+    ally_owner = models.PositiveIntegerField(default=0)
+    ally_register_time = models.IntegerField(default=0)
+    ally_description = models.TextField()
+    ally_web = models.CharField(max_length=255, default='')
